744dc6d627db8b41db9b788f5e210942cefdfa9b
Igor Brand <igorsca@gmail.com>
2015-03-27 03:30:20 -0300

    sample hackerspace statusbot

--- py/open_url_file.py
+++ py/open_url_file.py
@@ -4,6 +4,30 @@
 
 import urllib
 
-status = urllib.urlopen('http://garagemhacker.org/status.txt').read()
+status = urllib.urlopen('http://garagemhacker.org/status.txt').read().rstrip()
+fcomp = 'new3'
 
-print status
+
+try:
+	print 'Arquivo ja existe vou usar leitura direto'
+	f = open(fcomp, 'r')
+except:
+	print 'Arquivo nao existe vou crialo'
+	f = open(fcomp, 'a')
+	f.write(status)
+	f = open(fcomp, 'r')
+
+if f.read() == status:
+	print 'A garagem esta', status
+	print 'nao farei nada'
+	f.close()
+else:
+	print 'A garagem agora esta', status
+	f = open(fcomp, 'w')
+	f.write(status)
+	f = open(fcomp, 'r')
+	print 'Gravei novo estado'
+	f.close()
+
+
+
