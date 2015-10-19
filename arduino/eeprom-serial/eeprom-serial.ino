cb85c982ac8bad26d4c7511c04dd4df58cc46f81
Igor Brand <igorsca@gmail.com>
2015-10-19 17:39:44 -0200

    restart from sctrach eeprom-serial sample

--- arduino/eeprom-serial/eeprom-serial.ino
+++ arduino/eeprom-serial/eeprom-serial.ino
@@ -4,60 +4,32 @@
 */
 #include <EEPROM.h>
 
-int r,w;
-int addr = 0;
-byte value;
-int serialData = 0;
+char buffer[9];
+int r, w;
+int led = 13;
 
-void setup()
-{
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  pinMode(13, OUTPUT);  
+  Serial.flush();
+  pinMode(led, OUTPUT);
 }
-
-
-void ok()
-{
-    if (Serial.available() > 0) { 
-        
-      serialData = Serial.read();
-    
-      if (serialData == 49) { //just test to get 49 ASCII from serial and print
-          Serial.println("Ok, vc achou 1");
-          digitalWrite(13,1);
-          delay(500);
-          digitalWrite(13,0);
-          delay(500);  
-       }
- 
-      Serial.println(serialData);
-  }
-} //close ok function
-
-
-void read_ee()
-{
-  value = EEPROM.read(addr);
-  Serial.println("Adrress........DEC value");
-  Serial.print(addr);
-  Serial.print("\t");
-  Serial.println(value, DEC);
-  addr = addr +1;
-  if (addr == EEPROM.length()) {
-    addr = 0;
-  }
-  delay(250);
-}
-  
 
 void loop() {
-          if (Serial.available() > 0){
-            
-  
-  // read a byte from the current address of the EEPROM
-  ok();
-  read_ee();
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+        int index=0;
+        delay(100); //espaco pro buffer
+        int numChar = Serial.available();
+        if (numChar>9) {
+              numChar=9;
+        }
+        while (numChar--) {
+                buffer[index++] = Serial.read();
+        }
+  }
+
+
 }
+
+
