298d757a509ba733cd6557636ddc27afa55245e9
Igor Brand <igorsca@gmail.com>
2016-06-07 00:17:46 -0300

    more biped tests

--- arduino/bob-biped-wally/bob-biped-wally.ino
+++ arduino/bob-biped-wally/bob-biped-wally.ino
@@ -18,15 +18,18 @@ int potA; //prints of my pot
 int stepa;
 int pevai = 1;
 
-
 void setup() {
   
   Serial.begin(9600);
   rfoot.attach(3);
-
+  //lfoot.attach(5);
+  //rbut.attach(6);
+  //lbut.attach(9);
+  //lfoot.write(30);
+  
 }
 
-void loop() {
+void dance() {
 
   if (stepa == 0) {delay(1000); pevai = 1 ;}
   if ( stepa == 87) {delay(1000); pevai = -1;}
@@ -34,13 +37,32 @@ void loop() {
   stepa += pevai;
   Serial.println(stepa);
   rfoot.write(stepa);
+  lfoot.write(stepa);
   delay(10);
+}
+
+void potcontrol(){
+  
+  //poting controling two servos... rfoot and lfoot
+  potA = analogRead(potpin); //reading pot on potA
+  Serial.println(potA);
+  potA = map(potA, 0, 1023, 0, 180);
+  rfoot.write(potA);
+  //lfoot.write(potA);
+  //rbut.write(potA);
+  //lbut.write(potA);
+  delay(15);
+}
+
+void walk(){
   
   
-  //potA = analogRead(potpin); //reading pot on potA
-  //Serial.println(potA);
-  //potA = map(potA, 0, 1023, 0, 180);
-  //rfoot.write(potA);
-  //delay(15);  
+}
+
+void loop() {
+
+  //dance();
+  potcontrol();  
+  //walk();
 
 }
